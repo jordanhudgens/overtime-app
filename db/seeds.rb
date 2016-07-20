5fa8076996572f9aa9c0f0ce7d87c4a05ad098ba
Jordan Hudgens <jordan@wow.com>
2016-07-20 16:43:46 -0500

    Created seed file with default post content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} rationale content")
+end
+
+puts "100 Posts have been created"
