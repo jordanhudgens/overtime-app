67db13249034cd5954e780a1804c07eecd8ad2af
Jordan Hudgens <jordan@wow.com>
2016-07-20 17:32:22 -0500

    Implemented creation process for posts and refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show]
 	def index
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	private
+
+	  def post_params
+	  	params.require(:post).permit(:date, :rationale)
+	  end
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,22 @@ describe 'navigate' do
   		expect(page).to have_content(/Posts/)
   	end
   end
+
+  describe 'creation' do
+  	before do
+  		visit new_post_path
+  	end
+
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+  	end
+  end
 end
