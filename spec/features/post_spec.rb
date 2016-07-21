b8349e59e6d506c404ecc3a3501a21bf41e2f78e
Jordan Hudgens <jordan@wow.com>
2016-07-21 13:59:10 -0500

    Added new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
   	before do
   		visit new_post_path
