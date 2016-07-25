0f4300d3ded98493a1d6a6af436e35d833f1fca7
Jordan Hudgens <jordan@wow.com>
2016-07-25 17:16:33 -0500

    Implemented ability to block users after post marked complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || admin_types.include?(user.type)
+  	return true if post_approved? && admin?
+  	return true if user_or_admin && !post_approved?
   end
+
+  private
+
+  	def user_or_admin
+  		record.user_id == user.id || admin?
+  	end
+
+  	def admin?
+  		admin_types.include?(user.type)
+  	end
+
+    def post_approved?
+    	record.approved?
+    end
 end
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -28,5 +28,17 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
+    end
 	end
 end
