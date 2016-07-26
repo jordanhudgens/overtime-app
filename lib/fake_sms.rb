e7c605a1a8431c91440d1e405be12d8d2fbe3942
Jordan Hudgens <jordan@wow.com>
2016-07-26 16:24:03 -0500

    Created SMS stub for tests

---
+++ lib/fake_sms.rb
@@ -0,0 +1,12 @@
+module FakeSms
+  Message = Struct.new(:message, :message)
+  @messages = []
+
+  def self.send_sms(number:, message:)
+    @messages << Message.new(number, message)
+  end
+
+  def self.messages
+    @messages
+  end
+end
