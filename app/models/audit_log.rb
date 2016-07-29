9c3240cccc13dd79f9882d37418a21950047fa02
Jordan Hudgens <jordan@wow.com>
2016-07-29 14:49:19 -0500

    Integrated validations and defaults for audit log attributes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,14 @@
 class AuditLog < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :user_id, :status, :start_date
+
+  after_initialize :set_defaults
+
+
+  private
+
+    def set_defaults
+      self.start_date ||= Date.today - 6.days
+    end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  
+	before do
+		@audit_log = FactoryGirl.create(:audit_log)
+	end
+
+  describe 'creation' do
+  	it 'can be properly created' do
+  		expect(@audit_log).to be_valid
+  	end
+  end
+
+  describe 'validations' do
+  	it 'it should be required to have a user association' do
+  		@audit_log.user_id = nil
+  		expect(@audit_log).to_not be_valid
+  	end
+
+  	it 'it should always have a status' do
+  		@audit_log.status = nil
+  		expect(@audit_log).to_not be_valid
+  	end
+
+  	it 'it should be required to have a start_date' do
+  		@audit_log.start_date = nil
+  		expect(@audit_log).to_not be_valid
+  	end
+
+  	it 'it should have a start date equal to 6 days prior' do
+  		new_audit_log = AuditLog.create(user_id: User.last.id)
+  		expect(new_audit_log.start_date).to eq(Date.today - 6.days)
+  	end  	
+  end
 end
