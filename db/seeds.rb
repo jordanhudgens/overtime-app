b7f27a60656aa655146edd502fb0ccca2eb448f8
Jordan Hudgens <jordan@wow.com>
2016-07-29 15:17:13 -0500

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ end
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
