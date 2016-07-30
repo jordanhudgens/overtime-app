88bbec649aec627ee193a677b3d5d3ca5dbab116
Jordan Hudgens <jordan@wow.com>
2016-07-30 15:25:00 -0500

    Blocked non admin users from accessing index page for audits

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,5 +1,6 @@
 class AuditLogsController < ApplicationController
 	def index
 		@audit_logs = AuditLog.all
+		authorize @audit_logs
 	end
 end
--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -18,8 +18,14 @@ describe 'AuditLog Feature' do
 			expect(page).to have_content(/SNOW/)
 		end
 
-		xit 'cannot be accessed by non admin users' do
+		it 'cannot be accessed by non admin users' do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, :scope => :user)
 
+			visit audit_logs_path
+
+			expect(current_path).to eq(root_path)
 		end
 	end
 end
