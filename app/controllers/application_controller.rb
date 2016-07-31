8b9fc01fa2642da34ef6a7213099a94ea1c1ba5c
Jordan Hudgens <jordan@wow.com>
2016-07-30 19:10:48 -0500

    Setup skeleton employee homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def admin_types
+    ['AdminUser']
+  end
+
   private
 
   def user_not_authorized
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
 	def homepage
-		@pending_approvals = Post.where(status: 'submitted')
-		@recent_audit_items = AuditLog.last(10)
+		if admin_types.include?(current_user.type)
+			@pending_approvals = Post.submitted
+			@recent_audit_items = AuditLog.last(10)
+		else
+			#something else
+		end
 	end
 end
